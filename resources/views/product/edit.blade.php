81310c87b6c1195882611659df840892221cf4b5
ggx1234 <49161576+ggx1234@users.noreply.github.com>
2020-05-22 23:32:08 +0700

    thang/Product

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -1,1 +1,74 @@
-<?php
+@extends("layout")
+@section("title", "Update Product List")
+@section("contentHeader", "Update Product")
+@section("content")
+    <div class="row mt-4">
+        <div class="col">
+            <div class="card">
+                <!-- Card header -->
+                <div class="card-header border-0">
+                    <h2 class="mb-0 col-lg-9 float-left">Update Product</h2>
+                </div>
+                <!-- Light table -->
+                <form role="form" action="{{url("/update-product/{$product->__get("id")}")}}" method="post">
+                    @method("PUT")
+                    {{--            // method"POST" dùng để báo route--}}
+                    @csrf
+                    {{--            // dùng để tạo mã token nếu thiếu sẽ báo lỗi 419--}}
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label>Product Name</label>
+                            <input type="text" value="{{$product->__get("product_name")}}" name="product_name" class="form-control @error("product_name") is-invalid @enderror" placeholder="New Product Name">
+                            @error("product_name")
+                            <span class="error invalid-feedback">{{$message}}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label>Product Desc</label>
+                            <textarea name="product_desc" value="{{$product->__get("product_desc")}}" class="form-control @error("product_desc") is-invalid @enderror" placeholder="New Product Desc"></textarea>
+                            @error("product_desc")
+                            <span class="error invalid-feedback">{{$message}}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label>Price</label>
+                            <input type="text" name="price"  value="{{$product->__get("price")}}" class="form-control @error("price") is-invalid @enderror" placeholder="New Product Price">
+                            @error("price")
+                            <span class="error invalid-feedback">{{$message}}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label>Qty</label>
+                            <input type="text" name="qty" value="{{$product->__get("qty")}}" class="form-control @error("qty") is-invalid @enderror" placeholder="New Product Qty">
+                            @error("qty")
+                            <span class="error invalid-feedback">{{$message}}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label>Category</label>
+                            <select name="category_id" class="form-control">
+                                @foreach($categories as $category)
+                                    <option value="{{$category->__get("id")}}">{{$category->__get("category_name")}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Brand</label>
+                            <select name="brand_id" class="form-control">
+                                @foreach($brands as $brand)
+                                    <option value="{{$brand->__get("id")}}">{{$brand->__get("brand_name")}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        {{--                // biến error để lưu lỗi--}}
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+                <!-- Card footer -->
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/product/list.blade.php
+++ resources/views/product/list.blade.php
@@ -10,10 +10,6 @@
                     <h2 class="mb-0 col-lg-9 float-left">Product</h2>
                     <div class="mb-0 col-lg-3 float-right d-flex justify-content-end">
                         <a href="{{url("/new-product")}}" class="btn btn-sm btn-neutral">Create</a>
-                        @foreach($products as $product)
-                        <a href="{{url("/edit-product/{$product->__get("id")}")}}" class="btn btn-sm btn-neutral">Update</a>
-                        @endforeach
-
                     </div>
                 </div>
                 <!-- Light table -->
@@ -49,30 +45,36 @@
                                 data-sort="name">Updated At
                             </th>
                             <th scope="col" style="font-size: 14px; text-transform: capitalize!important;" class="sort"
+                                data-sort="name">Edit
+                            </th>
+                            <th scope="col" style="font-size: 14px; text-transform: capitalize!important;" class="sort"
                                 data-sort="name">Delete
                             </th>
                         </tr>
                         </thead>
                         <tbody class="list">
                         @foreach($products as $product)
-                        <tr>
-                            <td>{{$product->__get("id")}}</td>
-                            <td>{{$product->__get("product_name")}}</td>
-                            <td>{{$product->__get("product_desc")}}</td>
-                            <td>{{$product->__get("price")}}</td>
-                            <td>{{$product->__get("qty")}}</td>
-                            <td>{{$product->__get("category_id")}}</td>
-                            <td>{{$product->__get("brand_id")}}</td>
-                            <td>{{$product->__get("created_at")}}</td>
-                            <td>{{$product->__get("updated_at")}}</td>
-                            <td>
-                                <form action="{{url("/delete-product/{$product->__get("id")}")}}" method="post">
-                                    @method("DELETE")
-                                    @csrf
-                                    <button type="submit" onclick="return confirm('chac khong?');" class="btn btn-outline-dark">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
+                            <tr>
+                                <td>{{$product->__get("id")}}</td>
+                                <td>{{$product->__get("product_name")}}</td>
+                                <td>{{$product->__get("product_desc")}}</td>
+                                <td>{{$product->__get("price")}}</td>
+                                <td>{{$product->__get("qty")}}</td>
+                                <td>{{$product->__get("category_id")}}</td>
+                                <td>{{$product->__get("brand_id")}}</td>
+                                <td>{{$product->__get("created_at")}}</td>
+                                <td>{{$product->__get("updated_at")}}</td>
+                                <td>
+                                    <a href="{{url("/edit-product/{$product->__get("id")}")}}" class="btn btn-outline-dark">Edit</a>
+                                </td>
+                                <td>
+                                    <form action="{{url("/delete-product/{$product->__get("id")}")}}" method="post">
+                                        @method("DELETE")
+                                        @csrf
+                                        <button type="submit" onclick="return confirm('chac khong?');" class="btn btn-outline-dark">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
                         @endforeach
                         </tbody>
                     </table>
